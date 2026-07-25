39fa165d3372546aa2a9a5b99b98b03fbd9bf45b
Roshani836 <roshanibari836@gmail.com>
2026-07-25 11:32:47 +0530

    This is a loop code

---
+++ Day_6/even.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number = "))
+
+while n > 0:
+
+    if n % 2 == 0 :
+        print(n,"The number is even")
+    else:
+        print(n, "The number is odd")
+    n = n-1
---
+++ Day_6/pattern.py
@@ -0,0 +1,8 @@
+rows=5
+
+print(" Hash pattern code")
+
+for i in range(rows):
+    for j in range(rows):
+        print("#" , end=" " )
+    print()
