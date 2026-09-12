522330f5833af577b862d432c49bec45fc3d5088
Roshani836 <roshanibari836@gmail.com>
2026-09-12 10:43:22 +0530

    OOP codes

---
+++ Day_16/bank2.py
@@ -0,0 +1,43 @@
+class BankAccount:
+
+    def __init__(self, name, balance):
+        self.name = name
+        self.balance = balance
+
+    def deposite(self, amount ):
+        self.balance = self.balance + amount
+        print("Amount deposite: ", amount)
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance = self.balance - amount
+            print("Amount Withdraw: ", amount)
+        else:
+            print("Insufficient Balance")
+
+    def display(self):
+        print("Account Holder: ", self.name)
+        print("Balance: ", self.balance)
+
+account1 = BankAccount("Rahul", 5000)
+
+account1.display()
+
+print("\nDeposite: ")
+account1.deposite(2000)
+account1.display()
+
+print("\nWithdraw: ")
+account1.withdraw(3000)
+account1.display()
+
+print("\nWithdraw: ")
+account1.withdraw(1000)
+account1.display()
+
+
+
+
+
+
+    
